a178e192272daf1d4801aa95682c587cd2ef45ae
hgao234 <hgao234@wisc.edu>
2025-10-16 13:50:59 -0500

    Huajun modified school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv |
+grep 'MADISON SCHOOLS' |
+cut -d, -f7 |
+{
+  awk '{
+    sum += $1; n++
+  }
+  END {
+    printf "sum=%d\naverage=%.6f\n", sum, sum / n
+  }'
+}
