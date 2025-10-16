0e4117deea29284c2f51e92cba547a2a419e247b
che tian <ctian46@wisc.edu>
2025-10-16 13:29:01 -0500

    five_dirs.sh modification

---
+++ five_dirs.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+mkdir five
+cd five
+for i in {1..5}; do
+    mkdir "dir$i"
+    cd "dir$i"
+    for j in {1..4}; do
+	for k in $(seq 1 $j); do
+	    echo $j >> "file$j"
+	done
+    done
+    cd ..
+done
